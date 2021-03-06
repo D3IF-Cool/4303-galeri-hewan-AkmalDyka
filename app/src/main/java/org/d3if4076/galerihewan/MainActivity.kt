8be69fed85c964a2bee6ef615fd99d390c13a397
Akmal Dyka Baihaqi <akmaldykab@student.telkomuniversity.ac.id>
2021-03-06 09:30:19 +0700

    Menampilkan data dengan adapter

--- app/src/main/java/org/d3if4076/galerihewan/MainActivity.kt
+++ app/src/main/java/org/d3if4076/galerihewan/MainActivity.kt
@@ -2,10 +2,36 @@ package org.d3if4076.galerihewan
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
+import androidx.recyclerview.widget.DividerItemDecoration
+import androidx.recyclerview.widget.RecyclerView
+import org.d3if4076.galerihewan.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityMainBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        with(binding.recyclerView) {
+            addItemDecoration(DividerItemDecoration(context, RecyclerView.VERTICAL))
+            adapter = MainAdapter(getData())
+            setHasFixedSize(true)
+        }
+    }
+
+    private fun getData(): List<Hewan> {
+        return listOf(
+                Hewan("Angsa", "Cygnus olor"),
+                Hewan("Ayam", "Gallus gallus"),
+                Hewan("Bebek", "Cairina moschata"),
+                Hewan("Domba", "Ovis ammon"),
+                Hewan("Kalkun", "Meleagris gallopavo"),
+                Hewan("Kambing", "Capricornis sumatrensis"),
+                Hewan("Kelinci", "Oryctolagus cuniculus"),
+                Hewan("Kerbau", "Bubalus bubalis"),
+                Hewan("Kuda", "Equus caballus"),
+                Hewan("Sapi", "Bos taurus"),
+        )
     }
 }
