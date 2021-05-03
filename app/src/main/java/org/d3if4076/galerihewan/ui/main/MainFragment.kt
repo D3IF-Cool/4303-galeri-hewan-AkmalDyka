e2a46da77faaf230cbd1273aaf4ba1f3a4b2bee6
AkmalDykaB <akmaldykab@student.telkomuniversity.ac.id>
2021-05-03 21:09:33 +0700

    3.6.Menambahkan loading indicator

--- app/src/main/java/org/d3if4076/galerihewan/ui/main/MainFragment.kt
+++ app/src/main/java/org/d3if4076/galerihewan/ui/main/MainFragment.kt
@@ -11,6 +11,7 @@ import androidx.recyclerview.widget.RecyclerView
 import org.d3if4076.galerihewan.Hewan
 import org.d3if4076.galerihewan.R
 import org.d3if4076.galerihewan.databinding.FragmentMainBinding
+import org.d3if4076.galerihewan.network.HewanApi
 
 class MainFragment : Fragment() {
 
@@ -37,5 +38,23 @@ class MainFragment : Fragment() {
         viewModel.getData().observe(viewLifecycleOwner, {
             myAdapter.updateData(it)
         })
+        viewModel.getStatus().observe(viewLifecycleOwner, {
+            updateProgress(it)
+        })
     }
+    private fun updateProgress(status: HewanApi.ApiStatus) {
+        when (status) {
+            HewanApi.ApiStatus.LOADING -> {
+                binding.progressBar.visibility = View.VISIBLE
+            }
+            HewanApi.ApiStatus.SUCCESS -> {
+                binding.progressBar.visibility = View.GONE
+            }
+            HewanApi.ApiStatus.FAILED -> {
+                binding.progressBar.visibility = View.GONE
+                binding.networkError.visibility = View.VISIBLE
+            }
+        }
+    }
+
 }
--- app/src/main/java/org/d3if4076/galerihewan/ui/main/MainViewModel.kt
+++ app/src/main/java/org/d3if4076/galerihewan/ui/main/MainViewModel.kt
@@ -11,18 +11,23 @@ import org.d3if4076.galerihewan.R
 import org.d3if4076.galerihewan.network.HewanApi
 
 class MainViewModel : ViewModel() {
+    private val status = MutableLiveData<HewanApi.ApiStatus>()
     private val data = MutableLiveData<List<Hewan>>()
     init {
         retrieveData()
     }
     private fun retrieveData() {
         viewModelScope.launch {
+            status.value = HewanApi.ApiStatus.LOADING
             try {
                 data.value = HewanApi.service.getHewan()
+                status.value = HewanApi.ApiStatus.SUCCESS
             } catch (e: Exception) {
                 Log.d("MainViewModel", "Failure: ${e.message}")
+                status.value = HewanApi.ApiStatus.FAILED
             }
         }
     }
     fun getData(): LiveData<List<Hewan>> = data
+    fun getStatus(): LiveData<HewanApi.ApiStatus> = status
 }
